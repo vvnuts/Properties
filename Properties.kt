0d877379dc9d632003b656931a91b1a0a02e8e42
vvnuts <142222258+vvnuts@users.noreply.github.com>
2024-03-15 13:22:22 +0300

    Add files via upload

---
+++ Properties.kt
@@ -0,0 +1,44 @@
+class PropertyExample() {
+    var counter = 0
+    var propertyWithCounter: Int? = null
+        set(v) {
+            field = v
+            counter++
+        }
+}
+
+class LazyProperty(val initializer: () -> Int) {
+    var value: Int? = null
+    val lazy: Int
+        get() {
+            if (value == null) {
+                value = initializer()
+            }
+            return value!!
+        }
+}
+
+class LazyProperty(val initializer: () -> Int) {
+    val lazyValue: Int by lazy(initializer)
+}
+
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
+
+class D {
+    var date: MyDate by EffectiveDate()
+}
+
+class EffectiveDate<R> : ReadWriteProperty<R, MyDate> {
+
+    var timeInMillis: Long? = null
+
+    override fun getValue(thisRef: R, property: KProperty<*>): MyDate {
+        return timeInMillis!!.toDate()
+    }
+
+    override fun setValue(thisRef: R, property: KProperty<*>, value: MyDate) {
+        timeInMillis = value.toMillis()
+    }
+}
+
